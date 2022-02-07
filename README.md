945b3b165da9cc44494d4f9f48fb2815de42f3d1
landon-thull <landonthull.tx@gmail.com>
2022-02-07 11:55:58 -0600

    Restarted with regular react, added typescript

--- README.md
+++
@@ -1,7 +0,0 @@
-# Portfolio-1
-
-My first portfolio build, started after completing basic React course on CodeDamn
-
-## Build Tools
-
-- Gatsby
--- my-app/src/components/HeroSection.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function HeroSection() {
-  return (
-    <div>
-      <h1>Hero</h1>
-    </div>
-  );
-}
