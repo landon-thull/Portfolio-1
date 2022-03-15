ce43bf9f7ab569adfb34a58a2744a16972d49d35
landon-thull <landonthull.tx@gmail.com>
2022-03-14 22:40:04 -0500

    Built Contact Form, needs styling

--- my-app/src/components/Contact.tsx
+++ my-app/src/components/Contact.tsx
@@ -1,19 +1,65 @@
-import { Container } from "@mui/material";
+import React, { useState } from "react";
+import emailjs, { init } from "emailjs-com";
 
-export default function Contact() {
+init("AjjZZsi3UM9dVtkDv");
+
+const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [emailSent, setEmailSent] = useState(false);
+
+  const submit = () => {
+    if (name && email && message) {
+      const serviceId = "service_udpmfst";
+      const templateId = "template_daccss2";
+      const userId = "AjjZZsi3UM9dVtkDv";
+      const templateParams = {
+        name,
+        email,
+        message,
+      };
+
+      emailjs
+        .send(serviceId, templateId, templateParams, userId)
+        .then((response) => console.log(response))
+        .then((error) => console.log(error));
+
+      setName("");
+      setEmail("");
+      setMessage("");
+      setEmailSent(true);
+    } else {
+      alert("Please fill in all fields.");
+    }
+  };
+
   return (
-    <Container
-      sx={{
-        position: "relative",
-        width: "100vw",
-        height: "100vh",
-        justifyContent: "center",
-        alignItems: "center",
-        display: "flex",
-        color: "#F5F9E9",
-      }}
-    >
-      Contact page
-    </Container>
+    <div id="contact-form">
+      <input
+        type="text"
+        placeholder="Your Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="email"
+        placeholder="Your email address"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <textarea
+        placeholder="Your message"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      ></textarea>
+      <button onClick={submit}>Send Message</button>
+
+      <span className={emailSent ? "visible" : "invisible"}>
+        Thank you for your message, we will be in touch in no time!
+      </span>
+    </div>
   );
-}
+};
+
+export default Contact;
